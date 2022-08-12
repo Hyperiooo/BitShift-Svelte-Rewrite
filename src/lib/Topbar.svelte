8cf5ea00539f4fdbf7ce9a3238f123bbc59bad70
Noah <44048306+FakeDazon@users.noreply.github.com>
2022-08-12 00:54:06 -0700

    this is going to take so long lol

---
+++ src/lib/Topbar.svelte
@@ -0,0 +1,23 @@
+<header id="topbar">
+	<slot />
+</header>
+
+<style>
+	#topbar {
+		position: fixed;
+		top: 0;
+		padding: 0px;
+		left: 0;
+		width: 100%;
+		-webkit-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+		background: var(--bgDarkT);
+		backdrop-filter: saturate(180%) blur(20px);
+		-webkit-backdrop-filter: saturate(180%) blur(20px);
+		height: 50px;
+		display: flex;
+		align-items: center;
+	}
+</style>
