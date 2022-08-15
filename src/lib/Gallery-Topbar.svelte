85afcac6879a0cf86a781d1b3ea5a77245d51017
dazon <gamerpenguin2005@gmail.com>
2022-08-15 23:49:19 +0000

    a

---
+++ src/lib/Gallery-Topbar.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Topbar from "./Topbar.svelte"; import NavCenter from "./Nav-Center.svelte"; 
+    import Logo from "./assets/Logo.svelte"
+</script>
+<Topbar>
+    <NavCenter><Logo size=4></Logo></NavCenter>
+</Topbar>
---
+++ src/lib/Nav-Center.svelte
@@ -0,0 +1,23 @@
+<main id="nav-center"><slot /></main>
+ <style>
+    
+    #nav-center{
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%);
+    cursor: text;
+    font-size: 15px;
+    border-radius: 5px;
+    transition: 0.1s background, 0.3s color, 0.3s fill;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: none;
+    outline: none;
+    border: none;
+    color: var(--toolBarHover);
+    text-align: center;
+    background: var(--bgMain);
+    height: 36px;
+    }
+ </style>
